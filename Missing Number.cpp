a217b77131a3d830d226cb653573895535f5c13e
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-02-20 12:00:29 +0530

    Missing Number.cpp

---
+++ Missing Number.cpp
@@ -0,0 +1,8 @@
+ int n = nums.size();
+        int sum = n * ( n + 1) / 2;
+        int sum2 = 0;
+        for ( int i =0 ; i < n ; i++)
+        {
+            sum2 += nums[i]; 
+        }
+        return sum - sum2;
