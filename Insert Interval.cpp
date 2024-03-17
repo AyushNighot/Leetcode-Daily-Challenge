6c7dff137881ae514455f938beccafa41451e084
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-03-17 15:42:29 +0530

    Update Insert Interval.cpp

--- Insert Interval.cpp
+++ Insert Interval.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval)
+{
                 int i = 0;
         vector<vector<int>> result;
         
-        while(i < intervals.size()) {
-            if(intervals[i][1] < newInterval[0]) {
+        while(i < intervals.size())
+            {
+            if(intervals[i][1] < newInterval[0]) 
+            {
                 result.push_back(intervals[i]);
-            } else if (intervals[i][0] > newInterval[1]) { 
+            } else if (intervals[i][0] > newInterval[1]) 
+            { 
                 break;
-            } else {
+            } else
+            {
                 //Overlap : merge them
                 newInterval[0] = min(newInterval[0], intervals[i][0]);
                 newInterval[1] = max(newInterval[1], intervals[i][1]);
@@ -18,7 +23,8 @@ public:
         }
         
         result.push_back(newInterval);
-        while(i < intervals.size()) {
+        while(i < intervals.size()) 
+        {
             result.push_back(intervals[i]);
             i++;
         }
